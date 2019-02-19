144e7f6e5a2618c5369a3477315f809c41477ca1
Liva RAKOTOARINIA <liva.rakotoarinia@outlook.fr>
2019-02-19 14:34:02 +0100

    ajout des fichier de config + fichier source de l'app

---
+++ private/app/src/app.php
@@ -0,0 +1,8 @@
+<?php
+// Important de bien prendre les fichiers dans l'ordre (C'est de la logique algorithmique)
+require_once "../private/app/config/config.php";
+require_once "../private/app/src/environnement.php";
+require_once "../private/app/src/err_reporting.php";
+require_once "../private/app/src/db_connect.php";
+require_once "../private/app/src/routing.php";
+require_once "../private/app/src/compile.php";
